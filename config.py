342610a847cae2699591a4d5cf464f796747438b
raghavoo <155086740+raghavoo@users.noreply.github.com>
2024-01-11 11:41:09 +0530

    Update config.py
    Signed-off-by: raghavoo <155086740+raghavoo@users.noreply.github.com>

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://t.me/raghavsupport",
+    "https://github.com/raghavoo/AnonXMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
