b3d4e61279785ef03b40903218c91fb4fbd313e8
raghavoo <155086740+raghavoo@users.noreply.github.com>
2024-01-20 11:07:52 +0530

    Update start.py
    Signed-off-by: raghavoo <155086740+raghavoo@users.noreply.github.com>

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -31,7 +31,7 @@ def private_panel(_):
         ],
         [
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=_["S_B_7"], url=f"https://te.legra.ph/file/a246c58595fe1522a3d33.mp4"),
+            InlineKeyboardButton(text=_["S_B_7"], url=f"https://t.me/raghavsupport"),
         ],
     ]
     return buttons
