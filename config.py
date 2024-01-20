2abcd0f7aac12b75280d149825030926f19a26df
raghavoo <155086740+raghavoo@users.noreply.github.com>
2024-01-20 11:22:47 +0530

    Update config.py
    Signed-off-by: raghavoo <155086740+raghavoo@users.noreply.github.com>

--- config.py
+++ config.py
@@ -78,10 +78,10 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://te.legra.ph/file/c88624200de29f2d50cda.jpg"
+    "START_IMG_URL", "https://graph.org/file/cb99217f7d3aa7f954d66.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://te.legra.ph/file/c88624200de29f2d50cda.jpg"
+    "PING_IMG_URL", "https://graph.org/file/ae9eba3583b4c3940ac0a.jpg"
 )
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
 STATS_IMG_URL = "https://te.legra.ph/file/e906c2def5afe8a9b9120.jpg"
